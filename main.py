80b1010c16761bf49f4bcfd109a9d9ca8b8e76c8
Gaurav-pandeyy <gauravpande568@gmail.com>
2024-10-28 20:42:42 +0530

    Project File

--- main.py
+++ main.py
@@ -1,44 +1,53 @@
+from PyQt6.QtGui import QAction
+from PyQt6.QtWidgets import QApplication, QVBoxLayout, QLabel, QWidget, QGridLayout, QLineEdit, QPushButton, \
+    QMainWindow, QTableWidget, QTableWidgetItem,QDialog
 import sys
-from datetime import datetime
-from PyQt6.QtWidgets import QApplication, QVBoxLayout, QLabel, QWidget, QGridLayout, QLineEdit, QPushButton
+import sqlite3
 
-class AgeCalculator(QWidget):
+class MainWindow(QMainWindow):
     def __init__(self):
         super().__init__()
-        self.setWindowTitle("Age Calculator")
-        grid = QGridLayout()
+        self.setWindowTitle("Student Management System")
 
-        name_label = QLabel("Name")
-        self.name_line_edit = QLineEdit()
 
-        date_label = QLabel("Date of Birth (MM/DD/YYYY):")
-        self.DOB = QLineEdit()
-        calculate_button = QPushButton("Calculate Age")
-        self.output_label = QLabel("")
+        file_menu_item = self.menuBar().addMenu("&File")
+        help_menu_item = self.menuBar().addMenu("&Help")
 
-        grid.addWidget(name_label, 0, 0)
-        grid.addWidget(self.name_line_edit, 0, 1)
-        grid.addWidget(date_label, 1, 0)
-        grid.addWidget(self.DOB, 1, 1)
-        grid.addWidget(calculate_button, 2, 0, 1, 2)
-        grid.addWidget(self.output_label, 3, 0, 1, 2)
 
-        calculate_button.clicked.connect(self.calculate_age)
-        self.setLayout(grid)
+        add_student_action = QAction("Add Student",self)
+        add_student_action.triggered.connect(self.insert)
+        file_menu_item.addAction(add_student_action)
 
-    def calculate_age(self):
-        try:
-            current_year = datetime.now().year
-            date_of_birth = self.DOB.text()
-            birth_date = datetime.strptime(date_of_birth, "%m/%d/%Y").date()
-            age = current_year - birth_date.year
-            if datetime.now().date() < birth_date.replace(year=current_year):
-                age -= 1
-            self.output_label.setText(f"{self.name_line_edit.text()} is {age} years old.")
-        except ValueError:
-            self.output_label.setText("Please enter a valid date in MM/DD/YYYY format.")
+        about_action = QAction("About",self)
+        help_menu_item.addAction(about_action)
+        about_action.setMenuRole(QAction.MenuRole.NoRole)
+
+
+        self.table = QTableWidget()
+        self.table.setColumnCount(4)
+        self.table.setHorizontalHeaderLabels(("Id","Name","Course","Mobile"))
+        self.setCentralWidget(self.table)
+
+    def load_data(self):
+        connection = sqlite3.connect("database.db")
+        result = connection.execute("SELECT * FROM students")
+        self.table.setRowCount(0)
+        for row_number, row_data in enumerate(result):
+            self.table.insertRow(row_number)
+            for column_number,data in enumerate(row_data):
+                self.table.setItem(row_number,column_number,QTableWidgetItem(str(data)))
+        connection.close()
+
+    def inset(self):
+        dialog = InsertDialog()
+
+
 
 app = QApplication(sys.argv)
-age_calculator = AgeCalculator()
-age_calculator.show()
+main_Window = MainWindow()
+main_Window.show()
+main_Window.load_data()
 sys.exit(app.exec())
+
+
+
